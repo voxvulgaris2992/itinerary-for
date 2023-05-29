bcb7a3eec9d047b3f38358fa6a29776ba053ca2f
Raj Singh <singh4991raj@gmail.com>
2023-05-29 21:53:18 +0100

    adds latitude and longitude to itinerary model, converts address to latitude and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_180921) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_204221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_180921) do
     t.string "interests", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "places", force: :cascade do |t|
